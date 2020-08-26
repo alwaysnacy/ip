661419a3ce856d1e09d66b822081db58482609a1
alwaysnacy <tranglovemanga@gmail.com>
2020-08-26 13:06:37 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,19 +9,38 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        for (int i=0; i<20; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.print("-");
         }
         System.out.println();
         
         System.out.println("Hello! I'm Duke\n" + "What do you want to do today?");
-        for (int i=0; i<20; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.print("-");
         }
         System.out.println();
 
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        while (!line.equals("bye")) {
+            for (int i = 0; i < 20; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+            System.out.println(line);
+            for (int i = 0; i < 20; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+            line = sc.nextLine();
+        }
+
+        for (int i = 0; i < 20; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
         System.out.println("Bye. See you tomorrow!");
-        for (int i=0; i<20; i++) {
+        for (int i = 0; i < 20; i++) {
             System.out.print("-");
         }
         System.out.println();
