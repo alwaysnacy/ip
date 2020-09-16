48aca6996e05f8ee3e593cbb7536a574b4d70e41
alwaysnacy <tranglovemanga@gmail.com>
2020-09-16 20:19:16 +0800

    A-CodeQuality

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -3,25 +3,33 @@ public class TaskManager {
     private int numTasks =0;
 
     public void addTask(String task) {
-        String description = "";
-        String byTime = "";
-
         if (task.contains("todo")) {
-            description = task.split(" ", 2)[1];
-            tasks[numTasks] = new ToDo(description, numTasks);
+            addToDo(task);
         } else if (task.contains("deadline")) {
-            task = task.split(" ", 2)[1];
-            description = task.split("/by")[0].trim();
-            byTime =  task.split("/by")[1].trim();
-            tasks[numTasks] = new Deadline(description, numTasks, byTime);
+            addDeadline(task);
         } else if (task.contains("event")) {
-            task = task.split(" ", 2)[1];
-            description = task.split("/at")[0].trim();
-            byTime =  task.split("/at")[1].trim();
-            tasks[numTasks] = new Event(description, numTasks, byTime);
+            addEvent(task);
         }
-
         numTasks++;
+    }
+
+    public void addToDo(String task) {
+        String description = task.split(" ", 2)[1];
+        tasks[numTasks] = new ToDo(description, numTasks);
+    }
+
+    public void addDeadline(String task) {
+        task = task.split(" ", 2)[1];
+        String description = task.split("/by")[0].trim();
+        String byTime =  task.split("/by")[1].trim();
+        tasks[numTasks] = new Deadline(description, numTasks, byTime);
+    }
+
+    public void addEvent(String task) {
+        task = task.split(" ", 2)[1];
+        String description = task.split("/at")[0].trim();
+        String byTime =  task.split("/at")[1].trim();
+        tasks[numTasks] = new Event(description, numTasks, byTime);
     }
 
     public int getNumTasks() {
